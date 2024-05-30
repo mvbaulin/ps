057af2b0f18eeae26f17c3eea90b70ba839985f9
Maxim Baulin <mvbaulin@yandex.ru>
2024-05-30 20:49:21 +0300

    Restore prod

--- src/www/site/config/db_prod.php
+++ src/www/site/config/db_prod.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  "host" => "79.174.88.192",
-  "port" => 17598,
+  "host" => "localhost",
+  "port" => 3306,
   "username" => "admin",
-  "password" => "12345slK-",
+  "password" => "12345slK",
   "db" => "ps_store",
 ];
