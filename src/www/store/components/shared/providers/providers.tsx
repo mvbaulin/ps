ad13b6b19b13f8d4e074b42e744b98ff0331a501
Maxim Baulin <mvbaulin@yandex.ru>
2024-09-15 21:06:55 +0300

    Fixed scroll up

--- src/www/store/components/shared/providers/providers.tsx
+++ src/www/store/components/shared/providers/providers.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
 import NextTopLoader from 'nextjs-toploader';
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <SessionProvider>
